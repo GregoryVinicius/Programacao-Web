2f664f2b22579b04b2917eb4a779883dc4abffc6
GregoryVinicius <gregoryviniciuscla@gmail.com>
2024-07-08 22:22:44 -0300

    Oque eu errei?

--- Nova pasta/index.js
+++ Nova pasta/index.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     verificarTema();
-    localizacao();
+    verificarLocalizacaoStorage();
+    apresentarCurriculo();
+    lerCurriculo();
 });
 
-function localizacao(){
+function verificarLocalizacaoStorage(){
+    const localizacao = localStorage.getItem('localizacao');
+    if(localizacao){
+        document.getElementById("localizacao").value = localizacao;
+    } else{
+        getLocalizacao();
+    }
+}
+
+function getLocalizacao(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
             const lat = position.coords.latitude;
@@ -11,6 +22,7 @@ function localizacao(){
         fetch(`https://geocode.xyz/${lat},${long}?geoit=json`).then(response=>response.json()).then(data=>{
             const localizacao = data.region || data.city || `${lat},${long}`;
             document.getElementById("localizacao").value=localizacao;
+            localStorage.setItem("localizacao", localizacao);
         }).catch(error=>console.error("Alguma coisa deu errado", error));
     });
     }
@@ -28,4 +40,48 @@ function alterarTema() {
     const novoTema = tema == "dark" ? "ligth" : "dark";
     document.body.setAttribute("data-tema", novoTema);
     localStorage.setItem("tema", novoTema);
+}
+
+function gerarCV(){
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+    const localizacao = document.getElementById("localizacao").value;
+    const experiencia = document.getElementById("experiencia").value;
+    const habilidades = document.getElementById("habilidades").value;
+    const curriculo = {nome, email, telefone, localizacao, experiencia, habilidades};
+    console.log(curriculo);
+    localStorage.setItem("curriculo", JSON.stringify(curriculo));
+    apresentarCurriculo(curriculo);
+}
+
+function apresentarCurriculo(data){
+    const template = document.getElementById('templateCurriculo').contentEditable.cloneNode(true);
+    template.querySelector(".nome").textContent = data.nome;
+    template.querySelector(".email").textContent = data.email;
+    template.querySelector(".telefone").textContent = data.telefone;
+    template.querySelector(".localizacao").textContent = data.localizacao;
+    template.querySelector(".experiencia").textContent = data.experiencia;
+    template.querySelector(".habilidade").textContent = data.habilidade;
+
+    const mostrarCurriculo = document.getElementById("mostraCurriculo");
+    mostrarCurriculo.innerHTML = "";
+    mostrarCurriculo.appendChild(template);
+}
+
+function lerCurriculo(){
+    const curriculo = JSON.parse(localStorage.getItem("curriculo"));
+    if(curriculo){
+        apresentarCurriculo(curriculo);
+    }
+}
+
+function copiar(){
+    const curriculo = document.getElementById("mostrarCurriculo").textContent;
+    navigator.clipboard.writeText(curriculo).then(()=>{
+        alert("Conteudo copiado para a área de trasferência")
+    }).catch(error=>{
+        console.error("erro ao copiar", error);
+        alert("Erro ao copiar o conteúdo");
+    })
 }
